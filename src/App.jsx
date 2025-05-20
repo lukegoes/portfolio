af4a3681d526f47392fa74cbdaa9bc9d02d66f99
Lucas <luklg@outlook.com>
2025-05-20 12:21:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./sections/Hero"
+import Navbar from "./sections/Navbar"
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+export default App
